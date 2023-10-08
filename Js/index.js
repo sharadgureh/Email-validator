b4d2171e0a9eff1d22c56dfc0529804530561184
sharad gureh <infarno95@gmail.com>
2023-10-08 23:15:39 +0530

    final

--- Js/index.js
+++ Js/index.js
@@ -23,7 +23,7 @@ let result = {
 subtm.addEventListener("click", async (e) => {
     e.preventDefault()
     console.log("Clicked!")
-    resultcont.innerHTML = `<img width="123" src="/project/Email-validatior/image/loading.svg" alt="">`
+    resultcont.innerHTML = `<img width="123" src="C:\Users\infar\Desktop\desktop\work\website\project\Email-validator\image\loading.svg" alt="">`
     let key = "ema_live_hoMBlv3MIn2tcbmfIM5moCzMIM2dlmMa6ZuZqAmy";
     let email = document.getElementById("username").value 
     let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
